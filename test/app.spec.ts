3e26c62117611da903b78cb05848a9724cba7f42
Fábián Szabolcs <szabolcs2000@yahoo.com>
2020-11-07 00:42:07 +0100

    update user information

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -2,7 +2,7 @@ import { app } from '../src/server';
 import supertest from 'supertest';
 
 describe('Family Expense Tracker App', () => {
-  const user = { username: 'csongor', email: 'csongor@gmail.com', password: '12345678' };
+  const user = { username: 'csongor', email: 'csongor@gmail.com', password: 'password' };
 
   let requestHandle: supertest.SuperTest<supertest.Test>;
 
