b873f6821be5c8cedbdc7c8401035f67e4dae289
Csongor Szalai <csongor.szalai14@gmail.com>
2020-11-07 17:29:45 +0100

    Added basic Seed file and cofigurated test script

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,5 +1,4 @@
 import { Configuration, IDatabaseDriver, Options } from "@mikro-orm/core";
-import { env } from "process";
 import { Balance } from "./entities/balance";
 import { Family } from "./entities/family";
 import { FamilyMember } from "./entities/familymember";
@@ -8,6 +7,6 @@ import { User } from "./entities/user";
 
 export default {
   entities: [User, FamilyMember, Family, Balance, Invitation],
-  dbName: env.NODE_ENV === 'test' ? 'family-expense-tracker.test.sqlite' : 'family-expense-tracker.sqlite',
+  dbName: "family-expense-tracker.sqlite",
   type: "sqlite",
 } as Options<IDatabaseDriver> | Configuration<IDatabaseDriver>;
