07735db9fa7016e22421539aa48fe5fce8e1707b
Csongor Szalai <csongor.szalai14@gmail.com>
2020-11-29 23:30:49 +0100

    Added basic components

--- client-family-expense-tracker/src/app/app-routing.module.ts
+++ client-family-expense-tracker/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ExpenseComponent } from './expense/expense.component';
+import {SettingsComponent} from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ExpenseComponent },{ path: 'settings', component: SettingsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- client-family-expense-tracker/src/app/app.module.ts
+++ client-family-expense-tracker/src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpenseComponent } from './expense/expense.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ExpenseComponent, SettingsComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
