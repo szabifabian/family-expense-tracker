99e7001130fbc6c5b71d7d845397c1116274d7d4
Csongor Szalai <csongor.szalai14@gmail.com>
2021-01-09 22:31:06 +0100

    Updated menu permissions

--- client-family-expense-tracker/src/app/menu/menu.component.ts
+++ client-family-expense-tracker/src/app/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { AuthService } from '../core/services/auth.service';
 import { Observable } from 'rxjs';
+import { FamilymemberService } from '../core/services/familymember.service';
 
 @Component({
 	selector: 'app-menu',
@@ -11,11 +12,32 @@ import { Observable } from 'rxjs';
 export class MenuComponent {
 	isLoggedIn$: Observable<boolean>;
 
-	constructor(protected as: AuthService) {
+	constructor(protected as: AuthService, public members: FamilymemberService) {
 		this.isLoggedIn$ = as.isLoggedIn();
+		this.members.getMembers();
 	}
 
 	logout(): void {
 		this.as.logout();
 	}
+
+	checkMembers(): boolean {
+		return this.members.familymember$.getValue().length !== 0;
+	}
+
+	isAdmin(): boolean {
+
+		if(this.members.familymember$.getValue().length !== 0 && this.members.user$.getValue().length !== 0) {
+		  let findMyId = 0;
+	
+	
+		  while(this.members.familymember$.getValue()[findMyId].id !== this.members.user$.getValue()[0].id) {
+			findMyId++;
+		  }
+	
+		  return this.members.familymember$.getValue()[findMyId].role === "ADMIN";
+		} else {
+		  return false;
+		}
+	  }
 }
